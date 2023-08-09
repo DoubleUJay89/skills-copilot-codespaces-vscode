224a4b0fa6d72b073a352f6d3cf0d3d524e3c0c0
Willi <56823995+DoubleUJay89@users.noreply.github.com>
2023-08-09 16:43:01 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,77 @@
+// Create web server
+// Path: server.js
+// Compare this snippet from comments.js:
+// function calculateNumbers(var1, var2) {
+//   console.log(var1 + var2);
+// }
+// 
+// calculateNumbers(1, 100);
+// Compare this snippet from skills.js:
+// function skillsMember() {
+//   console.log("I'm a member of the skills module!");
+// }
+// Compare this snippet from member.js:
+// function skillsMember() {
+//   console.log("I'm a member of the skills module!");
+// }
+// 
+// module.exports = skillsMember;
+// Compare this snippet from app.js:
+// var skills = require("./skills");
+// var comments = require("./comments");
+// var server = require("./server");
+// 
+// skills();
+// comments();
+// server();
+// Compare this snippet from server.js:
+// var http = require("http");
+// 
+// function onRequest(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// }
+// 
+// http.createServer(onRequest).listen(8888);
+// console.log("Server has started.");
+// Compare this snippet from skills.js:
+// function skillsMember() {
+//   console.log("I'm a member of the skills module!");
+// }
+// 
+// module.exports = skillsMember;
+// Compare this snippet from comments.js:
+// function calculateNumbers(var1, var2) {
+//   console.log(var1 + var2);
+// }
+// 
+// calculateNumbers(1, 100);
+// Compare this snippet from member.js:
+// function skillsMember() {
+//   console.log("I'm a member of the skills module!");
+// }
+// 
+// module.exports = skillsMember;
+// Compare this snippet from app.js:
+// var skills = require("./skills");
+// var comments = require("./comments");
+// var server = require("./server");
+// 
+// skills();
+// comments();
+// server();
+// Compare this snippet from server.js:
+// var http = require("http");
+// 
+// function onRequest(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// }
+// 
+// http.createServer(onRequest).listen(8888);
+// console.log("Server has started.");
+// Compare this snippet from skills.js:
+// function skillsMember() {
+//   console.log("I'm a
